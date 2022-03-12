ae6c65d21714e441dc1276106a2b4c600f859457
Takeshi KISHIYAMA <kishiyama.t@gmail.com>
2022-03-12 13:28:31 +0900

    mphの段階で死んでるのが多すぎる

--- oneliner.sh
+++ oneliner.sh
@@ -15,6 +15,24 @@ else
             elif [ $2 == 1 ]; then
                 python3 prog/inf2model1.py $fname $2 >> $1
             elif [ $2 == 2 ]; then
+                # ここで失敗しているケースがある
+                # cat mph/Arabian01_00220.mph
+                # cat: mph/Arabian01_00220.mph: No such file or directory
+                # cat mph/Arabian01_00330.mph
+                # cat: mph/Arabian01_00330.mph: No such file or directory
+                # cat mph/Arabian01_00350.mph
+                # cat: mph/Arabian01_00350.mph: No such file or directory
+                # cat mph/Arabian01_00390.mph
+                # cat: mph/Arabian01_00390.mph: No such file or directory
+                # cat mph/Arabian01_00020.mph
+                # cat: mph/Arabian01_00020.mph: No such file or directory
+                # cat mph/Arabian01_00110.mph
+                # cat: mph/Arabian01_00110.mph: No such file or directory
+                # cat mph/Arabian01_00130.mph
+                # cat: mph/Arabian01_00130.mph: No such file or directory
+                # cat mph/Arabian01_00490.mph
+                # cat: mph/Arabian01_00490.mph: No such file or directory
+                # cat mph/Arabian01_01150.mph
                 python3 prog/inf2model1.py $fname $2 > mph/$fname.mph
                 python3 src/combine_morph_tree.py $fname >> $1
             else
